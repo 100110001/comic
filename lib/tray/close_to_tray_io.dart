6fd9057da426b3e74c018f1c7ba8459b2d580651
levi <levi@brindlechute.com>
2026-08-09 08:38:35 +0800

    [trivial] Restore graceful tray quit via tray/window destroy

--- lib/tray/close_to_tray_io.dart
+++ lib/tray/close_to_tray_io.dart
@@ -53,8 +53,8 @@ class _TrayHandler extends TrayListener {
         await windowManager.focus();
         break;
       case 'quit':
-        // 直接退出进程：等待插件销毁可能在回调中挂起，进程结束由系统清理托盘图标
-        exit(0);
+        await trayManager.destroy();
+        await windowManager.destroy();
     }
   }
 }
